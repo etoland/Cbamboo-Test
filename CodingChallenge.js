2b00df68776189b9ec1e6240ee5ebcd6724ad705
Emma Mandat-Toland <etoland007@gmail.com>
2024-04-08 13:05:32 +0100

    chore: fix missing logic

--- CodingChallenge.js
+++ CodingChallenge.js
@@ -6,17 +6,25 @@
 function countingSundays() {
 // sundayCount start at 0, and keep track of sundays
     let sundayCount = 0;
-// daysOfWeek where 0 is Sunday, but initialize at 1 for Monday
-    let daysOfWeek = 1;
+// daysOfWeek where 0 is Sunday, but initialize at 1 for Tuesday
+    let daysOfWeek = 2;
 // daysInMoths keeps track of days in each month (respectively - with feb calc below)
     let daysInMonths = [31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 // for loop to run through the years
     for (let y = 1901; y <= 2000; y++) {
         // calulcate if leap year or not based on gregorian calendar conditions
         daysInMonths[1] = 28 + (y % 4 === 0 && y % 400 === 0 || y % 100 !== 0);
-        // for of loop to
+        // for of loop to iterate over each 
         for (let daysInMonth of daysInMonths) {
-
+            // calc that shifts the day of the week to the next
+            daysOfWeek += daysInMonth % 7;
+            // if divisible by 7, its a sunday
+            if (daysOfWeek % 7 === 0) {
+                // then add to sunday count
+                sundayCount++
+            }
         }
     }
+    return sundayCount
 }
+console.log(countingSundays())
